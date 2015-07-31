f7f2ea8771f693d92f1a34af5404d3197bec09f7
Yannick Chabbert <yannick.chabbert@makina-corpus.com>
2015-07-31 14:50:37 +0200

    fix middleware redirect on back-office

--- mymoney/core/middleware.py
+++ mymoney/core/middleware.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.views import redirect_to_login
+from django.core.urlresolvers import get_script_prefix
 from django.shortcuts import resolve_url
 
 
@@ -11,5 +12,12 @@ class AnonymousRedirectMiddleware(object):
             path = request.get_full_path()
             resolved_login_url = resolve_url(settings.LOGIN_URL)
 
-            if not path.startswith(resolved_login_url):
-                return redirect_to_login(path, resolved_login_url)
+            # /login, /login?next=foo or /admin must skip redirect.
+            whitelist = (
+                resolved_login_url,
+                get_script_prefix() + settings.ADMIN_BASE_URL,
+            )
+            if path.startswith(whitelist):
+                return
+
+            return redirect_to_login(path, resolved_login_url)
--- mymoney/core/tests/test_middleware.py
+++ mymoney/core/tests/test_middleware.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import get_script_prefix, reverse
 from django.shortcuts import resolve_url
 from django.test import TestCase
 from django.utils.http import urlunquote
@@ -15,12 +15,14 @@ class AnonymousRedirectMiddlewareTestCase(TestCase):
         cls.owner = UserFactory(username='owner')
         cls.bankaccount = BankAccountFactory(owners=[cls.owner])
 
-    def test_(self):
+    def test_anonymous_redirect(self):
         url = reverse('banktransactions:list', kwargs={
             'bankaccount_pk': self.bankaccount.pk,
         })
         login_url = resolve_url(settings.LOGIN_URL)
+        admin_base_url = get_script_prefix() + settings.ADMIN_BASE_URL
 
+        # Anonymous should be redirect to login url.
         response = self.client.get(url, follow=True)
         self.assertEqual(response.request['PATH_INFO'], login_url)
         self.assertEqual(
@@ -28,15 +30,20 @@ class AnonymousRedirectMiddlewareTestCase(TestCase):
             'next=' + url
         )
 
-        # Check explicit infinite loop.
+        # Check explicit infinite loop with anonymous.
         response = self.client.get(login_url, follow=True)
         self.assertEqual(response.request['PATH_INFO'], login_url)
 
+        # However, check that anonymous could access back-office
+        response = self.client.get(admin_base_url, follow=True)
+        self.assertEqual(response.request['PATH_INFO'], admin_base_url + '/login/')
+
+        # Authentificated user are not redirected.
         self.client.login(username=self.owner, password='test')
         response = self.client.get(url, follow=True)
         self.assertEqual(response.request['PATH_INFO'], url)
 
-        # Check explicit infinite loop.
+        # Check explicit infinite loop after log out.
         logout_url = resolve_url(settings.LOGOUT_URL)
         response = self.client.get(logout_url, follow=True)
         self.assertEqual(response.request['PATH_INFO'], login_url)
