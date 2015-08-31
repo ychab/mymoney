a669ee66e62116169c89caf3485e8a9f839b0076
Yannick Chabbert <yannick.chabbert@gmail.com>
2015-08-31 10:28:38 +0200

    init gulp tools for js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('js', function () {
+  return gulp.src([
+        "./mymoney/static/bower_components/jquery/dist/jquery.js",
+        "./mymoney/static/bower_components/bootstrap/dist/js/bootstrap.js",
+        "./mymoney/static/bower_components/underscore/underscore.js",
+        "./mymoney/static/bower_components/moment/moment.js",
+        "./mymoney/static/bower_components/bootstrap-calendar/js/calendar.js",
+        "./mymoney/static/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js",
+        "./mymoney/static/bower_components/chartjs/Chart.js",
+        "./mymoney/core/static/core/js/core.js",
+        "./mymoney/apps/banktransactionanalytics/static/banktransactionanalytics/banktransactionanalytics.js",
+        "./mymoney/apps/banktransactions/static/banktransactions/banktransactions.js",
+    ])
+    .pipe(concat('mymoney.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./mymoney/static/dist/js/'));
+});
+
+gulp.task('default', ['js']);
