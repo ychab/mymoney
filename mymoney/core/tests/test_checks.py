328300b0105ad55b0542e85540e87ff31ca62017
Yannick Chabbert <yannick.chabbert@gmail.com>
2016-05-11 09:12:28 +0200

    review check test

--- mymoney/core/tests/test_checks.py
+++ mymoney/core/tests/test_checks.py
@@ -7,7 +7,7 @@ class AdminURLCheck(SimpleTestCase):
     @override_settings(MYMONEY={"ADMIN_BASE_URL": ''})
     def test_deploy_critical(self):
         errors = self.get_filtered_msgs(
-            run_checks(app_configs=['core'], include_deployment_checks=True)
+            run_checks(include_deployment_checks=True)
         )
         self.assertEqual(len(errors), 1)
         self.assertIsInstance(errors.pop(), Critical)
@@ -15,7 +15,7 @@ class AdminURLCheck(SimpleTestCase):
     @override_settings(MYMONEY={"ADMIN_BASE_URL": 'admin'})
     def test_deploy_warning(self):
         errors = self.get_filtered_msgs(
-            run_checks(app_configs=['core'], include_deployment_checks=True)
+            run_checks(include_deployment_checks=True)
         )
         self.assertEqual(len(errors), 1)
         self.assertIsInstance(errors.pop(), Warning)
@@ -23,14 +23,14 @@ class AdminURLCheck(SimpleTestCase):
     @override_settings(MYMONEY={"ADMIN_BASE_URL": 'foo'})
     def test_deploy_ok(self):
         errors = self.get_filtered_msgs(
-            run_checks(app_configs=['core'], include_deployment_checks=True)
+            run_checks(include_deployment_checks=True)
         )
         self.assertFalse(errors)
 
     @override_settings(MYMONEY={"ADMIN_BASE_URL": ''})
     def test_no_deploy(self):
         errors = self.get_filtered_msgs(
-            run_checks(app_configs=['core'], include_deployment_checks=False)
+            run_checks(include_deployment_checks=False)
         )
         self.assertFalse(errors)
 
