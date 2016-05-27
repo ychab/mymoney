a06cd2020c7a229d14492fbd4bba76982b860a2a
Yannick Chabbert <yannick.chabbert@gmail.com>
2016-05-27 13:21:17 +0200

    fix wsgi app

--- mymoney/wsgi.py
+++ mymoney/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mymoney.settings.production")
 
 application = get_wsgi_application()
